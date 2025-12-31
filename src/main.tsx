d1a377ba4921968a7d79c338903e2236471c79bc
unknown <2410631250065@student.unsika.ac.id>
2026-01-01 00:53:53 +0700

    fix github pages build

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./app/App";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  // GitHub Pages repo kamu:
   base: "/Laas-Photobooth/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  },
-  server: {
-    port: 5173,
-    strictPort: true,
   },
 });
